37536d05f9d00162e8b93df58bf07730a0fd6f50
chandang394 <chandangarg394@gmil.com>
2022-09-19 16:54:00 +0530

    chandan

---
+++ chndn.js
@@ -0,0 +1,29 @@
+let cars = ['BMW',"AUDI","BENZ",1,2,3,true];
+//  you can store any type of value in array just as above example 
+// console.log(cars);
+// console.table(cars);
+console.log(cars[5]);   // accessing yhe element 
+console.log(cars.length);
+cars[4] = "mahindra";  // replacing the element
+console.log(cars[4]);
+cars[7] = "Honda";
+console.log(cars[7]);
+
+
+
+let array2d = [ [1, 2, 3],    // 2d array accessing 
+             [5 ,"a", 6], 
+             [7,"abc",0]
+];
+
+console.log(array2d);
+console.log(array2d);
+console.table(array2d);
+console.log(array2d);
+console.table(array2d);
+console.log(array2d);
+console.table(array2d);console.log(array2d);
+console.table(array2d);console.log(array2d);
+console.table(array2d);console.log(array2d);
+console.table(array2d);console.log(array2d);
+console.table(array2d);
